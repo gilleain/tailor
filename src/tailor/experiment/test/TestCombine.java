d8f697162f4fcd47fcc826624816f1d2628728ad
gilleain <gilleain.torrance@gmail.com>
2026-03-28 20:50:30 +0000

    Test for combining lists

--- src/tailor/experiment/test/TestCombine.java
+++ src/tailor/experiment/test/TestCombine.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import org.junit.Test;
 
+import tailor.experiment.api.Sink;
+import tailor.experiment.api.Source;
+import tailor.experiment.operator.AtomListPipe;
 import tailor.experiment.operator.AtomPipe;
+import tailor.experiment.operator.CombineAtomLists;
 import tailor.experiment.operator.CombineAtoms;
 import tailor.experiment.operator.GroupPipe;
 import tailor.experiment.operator.PrintAtomLists;
 import tailor.experiment.operator.ScanAtomByLabel;
+import tailor.experiment.operator.ScanAtomListsByLabel;
+import tailor.structure.Atom;
 import tailor.structure.Chain;
 
 public class TestCombine {
@@ -19,7 +25,7 @@ public class TestCombine {
 	 * - Combine these into (O, N) pairs
 	 */ 
 	@Test
-	public void testCombine() {
+	public void testCombineAtoms() {
 		PrintAtomLists sink = new PrintAtomLists();
 		Chain chain = Helper.makeData();
 		GroupPipe inPipe = new GroupPipe(chain);
@@ -34,6 +40,34 @@ public class TestCombine {
 		
 		// run the pipeline
 		Helper.runAll(List.of(scanO, scanN, combineON));
+	}
+	
+	
+	/**
+	 * - Scan for 'N' and 'CA' atom pairs
+	 * - Scan for 'C' and 'O' atom pairs
+	 * - Combine these pairs into quads
+	 */
+	@Test
+	public void testCombineAtomLists() {
+		Chain chain = Helper.makeData();
+		GroupPipe inPipe1 = new GroupPipe(chain);	// TODO - annoying - otherwise need to reset iterator
+		GroupPipe inPipe2 = new GroupPipe(chain);
+		
+		AtomListPipe outPipe1 = new AtomListPipe();
+		ScanAtomListsByLabel scan1 = new ScanAtomListsByLabel(List.of("N", "CA"), inPipe1, outPipe1);
+		
+		AtomListPipe outPipe2 = new AtomListPipe();
+		ScanAtomListsByLabel scan2 = new ScanAtomListsByLabel(List.of("C", "O"), inPipe2, outPipe2);
+		
+		List<Source<List<Atom>>> sources = List.of(outPipe1, outPipe2);
+		CombineAtomLists combine12 = new CombineAtomLists(sources, new Sink<List<List<Atom>>>() {
+			// TODO - clearly a more general type needed
+			public void put(List<List<Atom>> item) {
+				System.out.println(item);
+			}
+		});
+		Helper.runAll(List.of(scan1, scan2, combine12));
 	}
 	
 }
